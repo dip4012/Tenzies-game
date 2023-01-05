6782c5713d0b0b7ae1452eac334ac8ecb328f4d0
Dip <dip4012@gmail.com>
2023-01-05 22:35:09 +0530

    Completed the basic version

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,71 @@
+import { useEffect, useState } from "react"
 import "./App.css"
 import Die from "./components/Die"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 function App() {
+	const [tenzies, setTenzies] = useState(false)
+	const [dice, setDice] = useState(allNewDice())
+
+	useEffect(() => {
+		const allHeld = dice.every((die) => die.isHeld)
+		const allSameValue = dice.every((die) => die.value === dice[0].value)
+		if (allHeld && allSameValue) {
+			setTenzies(true)
+			console.log("You won")
+		}
+	}, [dice])
+
+	function allNewDice() {
+		const newDice = []
+		for (let i = 0; i < 10; i++) {
+			newDice.push({
+				id: nanoid(),
+				value: Math.ceil(Math.random() * 6),
+				isHeld: false,
+			})
+		}
+		return newDice
+	}
+
+	function rollDice() {
+		if (tenzies) {
+			setTenzies(false)
+			setDice(allNewDice)
+		} else {
+			setDice((oldDice) =>
+				oldDice.map((die) =>
+					die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+				)
+			)
+		}
+	}
+
+	function holdDice(id) {
+		setDice((oldDice) =>
+			oldDice.map((die) => (die.id === id ? { ...die, isHeld: !die.isHeld } : die))
+		)
+	}
+
 	return (
 		<main>
-			<Die value={1} />
-			<Die value={1} />
-			<Die value={1} />
-			<Die value={1} />
-			<Die value={1} />
-			<Die value={1} />
-			<Die value={1} />
-			<Die value={1} />
-			<Die value={1} />
-			<Die value={1} />
+			{tenzies && (
+				<Confetti width={window.innerWidth} height={window.innerHeight} />
+			)}
+			<h1 className="title">Tenzies</h1>
+			<p className="instructions">
+				Roll until all dice are same. Click each die to freeze it at its current
+				value between rolls.
+			</p>
+			<div className="die-container">
+				{dice.map((die) => (
+					<Die key={die.id} die={die} holdDice={holdDice} />
+				))}
+			</div>
+			<button className="roll-btn" onClick={rollDice}>
+				{tenzies ? "New Game" : "Roll"}
+			</button>
 		</main>
 	)
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,7 +2,16 @@ import "./Die.css"
 import React from "react"
 
 function Die(props) {
-	return <div className="die">{props.value}</div>
+	return (
+		<div
+			className={`die ${props.die.isHeld ? "locked" : ""}`}
+			onClick={() => {
+				props.holdDice(props.die.id)
+			}}
+		>
+			{props.die.value}
+		</div>
+	)
 }
 
 export default Die
